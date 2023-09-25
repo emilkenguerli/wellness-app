0f7e8fa28f11d1f4562c9483f36378c65bd85d4a
emilkenguerli <64641696+emilkenguerli@users.noreply.github.com>
2023-09-25 12:18:54 +0200

    Delete auth server/keys.js

--- auth server/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mogoUrl:"mongodb://127.0.0.1:27017/sws",
-    jwtkey:"asasfefa",
-    GMAIL_USER: "drolimacoli@gmail.com",
-    GMAIL_PASS: "Lilgerald98",
-    EMAIL_SECRET: "bdaiwodnoiw",
-    RESET_PASSWORD_KEY: "hahahhaxd"
-}
